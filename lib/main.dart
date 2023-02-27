b525a8d1ff3dd31875d1646caa79da6ef72115f2
IT20131456 <it20131456@my.sliit.lk>
2023-02-27 13:10:37 +0530

    create Navigation Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,120 @@
-import 'package:fitness_mate/screens/home.dart';
 import 'package:flutter/material.dart';
+import 'my_drawer_header.dart';
+import 'screens/contacts.dart';
+import 'screens/dashboard.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fitness Mate',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Home(),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var currentPage = DrawerSections.dashboard;
+
+  @override
+  Widget build(BuildContext context) {
+    var container;
+    if (currentPage == DrawerSections.dashboard) {
+      container = DashboardPage();
+    } else if (currentPage == DrawerSections.contacts) {
+      container = ContactsPage();
+    }
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 126, 119, 255),
+        title: Text("Fitness Mate"),
+      ),
+      body: container,
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                MyHeaderDrawer(),
+                MyDrawerList(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget MyDrawerList() {
+    return Container(
+      padding: EdgeInsets.only(
+        top: 15,
+      ),
+      child: Column(
+        // shows the list of menu drawer
+        children: [
+          menuItem(1, "Dashboard", Icons.dashboard_outlined,
+              currentPage == DrawerSections.dashboard ? true : false),
+          menuItem(2, "Contacts", Icons.people_alt_outlined,
+              currentPage == DrawerSections.contacts ? true : false),
+        ],
+      ),
+    );
+  }
+
+  Widget menuItem(int id, String title, IconData icon, bool selected) {
+    return Material(
+      color: selected ? Colors.grey[300] : Colors.transparent,
+      child: InkWell(
+        onTap: () {
+          Navigator.pop(context);
+          setState(() {
+            if (id == 1) {
+              currentPage = DrawerSections.dashboard;
+            } else if (id == 2) {
+              currentPage = DrawerSections.contacts;
+            }
+          });
+        },
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Icon(
+                  icon,
+                  size: 20,
+                  color: Colors.black,
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+enum DrawerSections {
+  dashboard,
+  contacts,
 }
