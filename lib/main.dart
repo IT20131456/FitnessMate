917f6899a3ef811ed8a1ceb472c9553c30c44214
Loshith Hasinda Samararathna <it20127046@my.sliit.lk>
2023-03-20 01:14:49 +0530

    Update Reminder Notification Feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:fitness_mate/services/notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fitness_mate/screens/login_screen.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
+  NotificationService().initNotification();
+  tz.initializeTimeZones();
   runApp(MyApp());
 }
 
