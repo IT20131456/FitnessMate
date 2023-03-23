ce6d21cb28f7d6b6f0caba3aa043d330d6800bac
IT20131456 <it20131456@my.sliit.lk>
2023-03-24 00:46:20 +0530

    Create Dashbord page

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,9 @@
 import 'package:fitness_mate/services/notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:fitness_mate/screens/login_screen.dart';
 import 'package:timezone/data/latest.dart' as tz;
+import 'package:fitness_mate/screens/welcome_screen.dart';
 
-import 'package:fitness_mate/screens/workouts/workouts_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,7 +27,7 @@ class MyApp extends StatelessWidget {
 
         ),
       ),
-      home: LoginScreen(),
+      home: WelcomView(),
     );
   }
 }
